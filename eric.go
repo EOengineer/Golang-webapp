77d725c23b71acc8b8fd595dd853882a5422c664
EOligney <Eoligney@arthrex.com>
2015-07-15 16:51:11 -0400

    some routing work and starting to spitball auth

--- eric.go
+++ eric.go
@@ -15,25 +15,56 @@ func main() {
   http.Handle("/", rtr)
 
   // routes
-  rtr.HandleFunc("/services", serviceHandler)
-  rtr.HandleFunc("/services/{key}/resources", resourceHandler)
+  rtr.HandleFunc("/login", loginHandler)
+  rtr.HandleFunc("/services", servicesHandler)
+  rtr.HandleFunc("/services/{name}", serviceHandler)
+  rtr.HandleFunc("/services/{s_name}/resources", resourcesHandler)
+  rtr.HandleFunc("/services/{s_name}/resources/new", newResourceHandler)
 
   // some server feedback
   log.Println("Listening...")
   http.ListenAndServe(":3000", nil)
 }
 
-
-func serviceHandler(w http.ResponseWriter, r *http.Request) {
-  // params := mux.Vars(r)
-  p := "whatever"
-  // key := params["key"]
-  t, _ := template.ParseFiles("services.html")
-  t.Execute(w, p)
+type Service struct {
+  Name string
 }
 
-func resourceHandler(w http.ResponseWriter, r *http.Request) {
-  p := "Whatever"
-  t, _ := template.ParseFiles("resources.html")
-  t.Execute(w, p)
+type Resource struct {
+  Name string
+}
+
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+  if r.Method == "GET" {
+    t, _ := template.ParseFiles("views/login.html")
+    t.Execute(w, nil)
+  } else {
+    // delegate to some method that performs the auth check
+   // and conditional redirect on success 
+  // authCheck(w, r)
+  }
+}
+
+func servicesHandler(w http.ResponseWriter, r *http.Request) {
+  t, _ := template.ParseFiles("views/services/index.html")
+  t.Execute(w, nil)
+}
+
+func serviceHandler(w http.ResponseWriter, r *http.Request) {
+  vars := mux.Vars(r)
+  s := &Service{Name: vars["name"]}
+  t, _ := template.ParseFiles("views/services/show.html")
+  t.Execute(w, s)
+}
+
+func resourcesHandler(w http.ResponseWriter, r *http.Request) {
+  vars := mux.Vars(r)
+  s := &Service{Name: vars["s_name"]}
+  t, _ := template.ParseFiles("views/resources/index.html")
+  t.Execute(w, s)
+}
+
+func newResourceHandler(w http.ResponseWriter, r *http.Request) {
+  t, _ := template.ParseFiles("views/resources/new.html")
+  t.Execute(w, nil)
 }
